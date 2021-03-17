4aea11933b82659f7e389b41b50731457ef8e2b8
Juan Barrientos <jbarr078@ucr.edu>
2021-03-17 12:46:45 -0700

    added comments throughout all functions

--- driver.py
+++ driver.py
@@ -4,6 +4,10 @@ import timeit
 from copy import deepcopy
 
 def feature_search_demo(data_list):
+    """
+    Forward selection. Starts with empty set, and progressively adds more features
+    to the set as they provide an increased accuracy.
+    """
     best_accuracy_so_far = 0
     current_set_of_features = set()
     current_dict_of_features = {}
@@ -18,14 +22,20 @@ def feature_search_demo(data_list):
         for k in range(1, num_features):
             if k not in current_set_of_features:
                 accuracy = leave_one_out_cross_validation(data_list, current_set_of_features, k, num_lines, num_features)
+                # deepcopy to print this out, can be optimized a bit better
                 print_set = deepcopy(current_set_of_features)
                 print_set.add(k)
                 print(f'Using feature(s) %s accuracy is {accuracy * 100:.2f}%%' % (print_set))
 
+                # If the accuracy from cross validation is better, update the 
+                # feature to add and its accuracy
                 if accuracy > best_accuracy_so_far:
                     best_accuracy_so_far = accuracy
                     feature_to_add_at_this_level = k
         
+        # If the feature provides an increase in accuracy, add it to the set
+        # Otherwise stop because all features are added or accuracy is 
+        # decreasing
         if feature_to_add_at_this_level is not None:
             current_set_of_features.add(feature_to_add_at_this_level)
             current_dict_of_features[feature_to_add_at_this_level] = best_accuracy_so_far
@@ -42,6 +52,10 @@ def feature_search_demo(data_list):
     return current_dict_of_features
 
 def backward_search(data_list):
+    """
+    Backward elimination. Start with a set of all possible features and remove
+    a feature at every level based on the overall accuracy.
+    """
     num_lines = len(data_list)
     num_features = len(data_list[0])
     current_set_of_features = set(range(1, num_features))
@@ -55,15 +69,20 @@ def backward_search(data_list):
 
         for k in range(1, num_features):
             if k in current_set_of_features:
+                # We use -1 * k to denote backward elimination 
                 accuracy = leave_one_out_cross_validation(data_list, current_set_of_features, (-1 * k), num_lines, num_features)
+                # deepcopy to print this out, can be optimized a bit better
                 print_set = deepcopy(current_set_of_features)
                 print_set.remove(k)
                 print(f'Using feature(s) %s accuracy is {accuracy * 100:.2f}%%' % (print_set))
-
+                # If the accuracy from cross validation is better, update the 
+                # feature to add and its accuracy
                 if accuracy > best_accuracy_so_far:
                     best_accuracy_so_far = accuracy
                     feature_to_remove_at_this_level = k
-        
+        # If the feature provides an increase in accuracy, add it to the set
+        # Otherwise stop because all features are added or accuracy is 
+        # decreasing        
         if feature_to_remove_at_this_level is not None:
             current_set_of_features.remove(feature_to_remove_at_this_level)
             current_dict_of_features[feature_to_remove_at_this_level] = best_accuracy_so_far
@@ -81,13 +100,21 @@ def backward_search(data_list):
     
 
 def leave_one_out_cross_validation(data, current_set, feature, num_lines, num_features):
-    # If forward search, append the feature to add
+    """
+    Performing the nearest neighbor search. Uses the features provided to 
+    determine an accuracy.
+    """
+    # If feature is a positive value, it is a forward search so we append the 
+    # feature
+    # Otherwise, if feature is a negative value, it is a backward elimination so
+    # we remove the feature
     cross_check = deepcopy(list(current_set))
     if feature > 0:
         cross_check.append(feature)
     else:
         cross_check.remove(abs(feature))
 
+    # Create a deepcopy of the data and then set all unnecessary features to 0
     cross_data = deepcopy(data)
     for i in range(0, num_lines):
         for k in range(1, num_features):
@@ -96,6 +123,8 @@ def leave_one_out_cross_validation(data, current_set, feature, num_lines, num_fe
 
     number_correctly_classified = 0
 
+    # Calculating the nearest neighbor distance and classifying each instance
+    # then using the correctly classified values to calculate accuracy
     for i in range(0, num_lines):
         object_to_classify = cross_data[i][1:]
         label_object_to_classify = cross_data[i][0]
@@ -105,16 +134,12 @@ def leave_one_out_cross_validation(data, current_set, feature, num_lines, num_fe
 
         for k in range(0, num_lines):
             if k != i:
-                # print(f'Ask if %s is nearest neighbor with %s' % (i, k))
                 distance = np.sqrt(sum([(a - b) * (a - b) for a, b in zip(object_to_classify, cross_data[k][1:])]))
 
                 if distance < nearest_neighbor_distance:
                     nearest_neighbor_distance = distance
                     nearest_neighbor_location = k
                     nearest_neighbor_label = cross_data[nearest_neighbor_location][0]
-
-        # print(f'Object %s is class %s' % (i + 1, label_object_to_classify))
-        # print(f'Its nearest neighbor is %s which is in class %s' % (nearest_neighbor_location, nearest_neighbor_label))
 
         if label_object_to_classify == nearest_neighbor_label:
             number_correctly_classified = number_correctly_classified + 1
@@ -124,6 +149,9 @@ def leave_one_out_cross_validation(data, current_set, feature, num_lines, num_fe
     return accuracy 
 
 def selection():
+    """
+    This gathers the user input such as file name and algorithm selection.
+    """
     while True:
         file_selection = input('Type in the name of the file to test: ')
         break
